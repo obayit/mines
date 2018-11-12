2fbbb5a6339525a5628b8f46d86db8d03365fa96
obi <obi@sudani.sd>
2018-11-12 16:02:55 +0200

    daily commit

---
+++ Controllers/MinesController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using mines.model;
+
+namespace mines.Controllers
+{
+    [Route("api/[controller]")]
+    public class MinesController : Controller
+    {
+        public class MinesInfo{
+            public int width;
+            public int height;
+            public int minesCount;
+            public int[][] mines;
+        }
+        [HttpGet("[action]")]
+        public JsonResult getMines(){
+            var m = new Mines(10, 10, 15);
+            m.initMine();
+            var res = new MinesInfo();
+            res.width = m.width;
+            res.height = m.height;
+            res.minesCount = m.minesCount;
+            res.mines = m.mines;
+            return new JsonResult(res);
+        }
+    }
+}
